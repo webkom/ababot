e03b43e4b60a52918e3b5b1219faae420a346851
Marius Kotlarz <marius.kotlarz@gmail.com>
2017-05-01 14:37:36 +0200

    Add pizza argument logic, fetch member name from medlemmer and add logic for order verification

--- scripts/pizza.js
+++ scripts/pizza.js
@@ -2,7 +2,9 @@ const Promise = require('bluebird');
 const path = require('path');
 const Nexmo = require('nexmo');
 const express = require('express');
+const moment = require('moment');
 const polly = require('../lib/polly');
+const members = require('../lib/members');
 
 const {
   NEXMO_API_KEY,
@@ -39,38 +41,147 @@ const call = () => calls.createAsync({
     answer_url: [`${URL}/answer`]
   });
 
+function generateErrorMessage(errorMessage) {
+  return 'Feil: ' + errorMessage + '\n\n' +
+         'Bestillingsformat: ' + pizzaOrderFormat;
+}
+
+function getPizzaName(slackName) {
+  return members(`?slack=${slackName}`)
+    .then((body) => {
+      const user = body[0];
+
+      if (!user || !user.name) {
+        throw new Error(`Fant ikke ${slackName} i medlemmer.`);
+      }
+
+      return user.name.split(' ', 1)[0]; // Get first name only
+    });
+}
+
 module.exports = (robot) => {
   robot.router.use(`/${EXPRESS_STATIC}`, express.static(path.join(__dirname, '..', EXPRESS_STATIC)))
 
-  robot.respond(/pizza/i, (msg) => {
-    polly({
-        pizzas: [12, 22],
-        pickupTime: '12:30',
-        name: 'Webkom'
-      })
-      .then(() => call())
-      .then((data) => {
-        store[data.conversation_uuid] = {
-          channel: msg.message.room,
-          timestamp: msg.message.id
+  robot.react((msg) => {
+    if (msg.message.type == 'added'
+      && msg.message.item_user.name == 'hubot'
+      && msg.message.reaction == 'white_check_mark'
+      && msg.message.room in currentOrders
+      && (Math.floor(Date.now() / 1000) - currentOrders[msg.message.room].time) <= 60) { // Must react within 1 minute
+        const currentOrder = currentOrders[msg.message.room];
+        polly({
+            pizzas: currentOrder.pizzas,
+            pickupTime: currentOrder.pickupTime,
+            name: currentOrder.name
+          }) // FIXME: just pass the whole currentOrder Object?
+          .then(() => call())
+          .then((data) => {
+            store[data.conversation_uuid] = {
+              channel: msg.message.room,
+              timestamp: msg.message.id
+            };
+            robot.adapter.client.web.reactions.add(
+              'pizza',
+              {
+                channel: msg.message.room,
+                timestamp: msg.message.id
+              }
+            )
+          })
+          .catch((err) => {
+            robot.adapter.client.web.reactions.add(
+              'x',
+              {
+                channel: msg.message.room,
+                timestamp: msg.message.id
+              }
+            )
+          })
+        );
+    }
+  });
+
+  robot.respond(/pizza ( .*)?/i, (msg) => {
+    const send = msg.send.bind(msg);
+    const slackName = msg.match[1] ? msg.match[1].trim() : msg.message.user.name;
+      getPizzaName(slackName)
+      .then((name) => {
+        const orderArray = msg.message.text.split(' ');
+        // Remove the first two items (bot name & command)
+        orderArray.splice(0, 2);
+
+        // Too many or too few arguments
+        if (orderArray.length !== 2) {
+          msg.send(generateErrorMessage('For få argumenter (.length !== 2)'))
+          return;
+        }
+
+        // Check pickup time
+        const pickupTimeString = orderArray.splice(-1, 1);
+        let orderPickupTime = moment(pickupTimeString, 'HH:mm');
+        if (!orderPickupTime.isValid()) {
+          msg.send(generateErrorMessage(
+            'Klokkeslettet for henting må være i følgende format: HH:mm'
+          ));
+          return;
+        }
+        orderPickupTime = orderPickupTime.format('HH:mm');
+
+        // Check if the pizzas are valid
+        const pizzaIds = orderArray[0].split(',');
+        const orderPizzas = {};
+        for (var i = 0, len = pizzaIds.length; i < len; i++) {
+          if (isNaN(pizzaIds[i])) {
+            // ID is not a number
+            msg.send(generateErrorMessage(
+              pizzaIds[i] + ' er ikke et gyldig pizza nummer'
+            ));
+            return;
+          }
+
+          const pizzaId = parseInt(pizzaIds[i], 10);
+
+          if (pizzaId < 1 || pizzaId > 28) {
+            // Out of pizza id range
+            msg.send(generateErrorMessage(
+              'Pizza nummer må være fra 1 til og med 28, "' +
+              pizzaId + '" er utenfor'
+            ));
+            return;
+          }
+
+          if (orderPizzas[pizzaId]) {
+            orderPizzas[pizzaId] += 1;
+          } else {
+            orderPizzas[pizzaId] = 1;
+          }
+        }
+
+        // Format the order to a pretty string
+        const orderTextPretty = Object.keys(orderPizzas).map((pizzaId) => {
+          const count = orderPizzas[pizzaId];
+          return `${count}x ${pizzaId}`;
+        });
+
+        // Set the current order
+        currentOrders[msg.message.room] = {
+          id: msg.message.id,
+          room: msg.message.room,
+          time: Math.floor(msg.message.id),
+          name: name,
+          pickupTime: orderPickupTime,
+          pizzas: pizzaIds
         };
-        robot.adapter.client.web.reactions.add(
-          'pizza',
-          {
-            channel: msg.message.room,
-            timestamp: msg.message.id
-          }
-        )
+
+        // Send a confirmation message
+        msg.send(
+          'React med :white_check_mark: på denne meldingen ' +
+          'for å bekrefte bestillingen på ' + pizzaIds.length +
+          ' pizza: ' + orderTextPretty.join(', ') +
+          ' som skal hentes kl. ' + orderPickupTime
+       );
       })
-      .catch((err) => {
-        robot.adapter.client.web.reactions.add(
-          'x',
-          {
-            channel: msg.message.room,
-            timestamp: msg.message.id
-          }
-        )
-      })
+      .catch((error) => send(error.message))
   });
 
   robot.router.get('/answer', (req, res) => {
