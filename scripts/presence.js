4c11a30c84ba353edd99188f898459bc55286caf
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-09-13 18:36:12 +0200

    Post message to slack

--- scripts/presence.js
+++ scripts/presence.js
@@ -3,13 +3,15 @@
 
 const token = process.env.PRESENCE_TOKEN;
 const members = require('../lib/members');
+const log = console.log;
 
 module.exports = (robot) => {
   robot.router.post('/presence', (request, response) => {
-    const authorization = request.headers.AUTHORIZATION;
+    const authorization = request.headers.authorization;
 
     if (token !== authorization) {
-      return response.json({ details: 'Invalid token' });
+      log(`Token denied, ${authorization}`);
+      return response.status(401).json({ details: 'Invalid token' });
     }
 
     const { rfid } = request.body.payload ? request.body.payload : request.body;
@@ -19,10 +21,11 @@ module.exports = (robot) => {
       const user = body[0];
 
       if (!user || !user.github) {
-        return response.json({ details: `${rfid} not found` });
+        return response.status(400).json({ details: `${rfid} not found` });
       }
 
-      console.log('SUCCESS :)')
+      robot.messageRoom('#general', `${user.github} entered the office!`);
+
       return response.json({ details: `Success` });
     });
 
