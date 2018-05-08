f34230d84e615a086c9846623b28061ccbcf26fa
Jonas <jonasxb@gmail.com>
2018-05-09 00:41:13 +0200

    Edit regex to match new lines as well as quotation marks

--- scripts/poll.js
+++ scripts/poll.js
@@ -2,14 +2,15 @@
 //   Create poll
 // Commands
 //   hubot poll "<pollName>" "<option0>" "<option1>" "<option2>" ... - Create and post poll with given options
+//   hubot poll\n<pollName>\n<option0>\n<option1>... - Create poll with new lines instead of quotation marks
 
 const _ = require('lodash');
 
 const defaultReaction = 'x';
 
 const createPoll = (message, reactionPool) => {
-  const [name, ...options] = (message.match(/\".+?\"/g) || []).map(string =>
-    string.replace(/\"/g, '')
+  const [name, ...options] = (message.match(/(\".+?\")|(\n.+)/g) || []).map(
+    string => string.replace(/\"|\n/g, '')
   );
   return {
     name,
@@ -45,7 +46,7 @@ module.exports = robot => {
         if (poll.name && poll.options.length >= 2) {
           const response = createResponse(poll);
           robot.adapter.client.web.chat
-            .postMessage(msg.message.room, response, { username: 'hubot' })
+            .postMessage(msg.message.room, response, { username: 'PaaS' })
             .then(r => {
               if (r.ok)
                 poll.options
