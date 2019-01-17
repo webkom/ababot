2e8c00ca8e02916d33fc7e15c927c6edf63b62e7
Peder Smith <smith.peder@gmail.com>
2019-01-17 17:33:52 +0100

    FIX

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -5,13 +5,13 @@ const _ = require('lodash');
 const members = require('../lib/members');
 
 module.exports = robot => {
-  robot.hear(/@nummer/i, msg => {
-    members('active=true')
+  robot.hear(/nummer/i, msg => {
+    members('?active=true')
       .then(members => {
         if (members.length === 0) {
           return;
         }
-        msg.send(members.map(m => `${m.name}: ${m.phone_number}`));
+        msg.send(members.map(m => `${m.name}: ${m.phone_number}\n`));
       })
       .catch(error => msg.send(error.message));
   });
