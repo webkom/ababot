f1b550565b719b566e58fc8209243daf8703a574
Peder Smith <smith.peder@gmail.com>
2019-01-17 17:22:25 +0100

    Add numbers script to hubot

---
+++ scripts/numbers.js
@@ -0,0 +1,18 @@
+// Description:
+// Get all phone numbers for active members
+
+const _ = require('lodash');
+const members = require('../lib/members');
+
+module.exports = robot => {
+  robot.hear(/@nummer/i, msg => {
+    members('active=true')
+      .then(members => {
+        if (members.length === 0) {
+          return;
+        }
+        msg.send(members.map(m => `${m.name}: ${m.phone_number}`));
+      })
+      .catch(error => msg.send(error.message));
+  });
+};
