438e6b49e171e8db87e1982677861b6c61dfcdad
Peder Smith <smith.peder@gmail.com>
2019-04-18 18:00:25 +0200

    Remove duplicate posts by ababot

--- scripts/nummer.js
+++ scripts/nummer.js
@@ -1,75 +1,31 @@
 // Description:
-//   Pick and tag a random user that has to do shitty work, replies when the bot
-//   hears @noen. This script also supports mentions of @aktive and @nye.
+// Get all phone numbers for active members
 
 const _ = require('lodash');
 const members = require('../lib/members');
 
-const prefixes = [
-  'Time to shine',
-  "The work doesn't do itself",
-  'Get going',
-  'lol rekt',
-  'About time you got picked',
-  'RIP',
-  'Move it'
-];
-
-const createMention = username => `@${username}`;
-
 module.exports = robot => {
-  robot.hear(/@noen-nye/i, msg => {
-    members('?new=true')
-      .then(members => {
-        if (!members.length) {
-          return;
-        }
-
-        const luckyMember = _.sample(members);
-        const mention = createMention(luckyMember.slack);
-        const cheesyPrefix = _.sample(prefixes);
-        msg.send(`${cheesyPrefix} ${mention}`);
-      })
-      .catch(error => msg.send(error.message));
-  });
-
-  robot.hear(/@noen/i, msg => {
-    // Reply with a cheesy message and a random picked mention.
-    members('?active=true')
-      .then(members => {
-        if (!members.length) {
-          return;
-        }
-
-        const luckyMember = _.sample(members);
-        const mention = createMention(luckyMember.slack);
-        const cheesyPrefix = _.sample(prefixes);
-        msg.send(`${cheesyPrefix} ${mention}`);
-      })
-      .catch(error => msg.send(error.message));
-  });
-
-  robot.hear(/@aktive|@active/i, msg => {
-    // Reply with a message containing mentions of all active users.
+  robot.respond(/nummer/i, msg => {
     members('?active=true')
       .then(members => {
         if (members.length === 0) {
           return;
         }
-
-        msg.send(members.map(member => createMention(member.slack)).join(', '));
-      })
-      .catch(error => msg.send(error.message));
-  });
-
-  robot.hear(/@nye/i, msg => {
-    members('?new=true')
-      .then(members => {
-        if (members.length === 0) {
-          return;
-        }
-
-        msg.send(members.map(member => createMention(member.slack)).join(', '));
+        msg.send(
+          members
+            .map(
+              m =>
+                `*${m.name}*:${'\t'} ${m.phone_number.substr(
+                  0,
+                  3
+                )} ${m.phone_number
+                  .substr(3)
+                  .match(/.{1,2}/g)
+                  .join(' ')}`
+            )
+            .sort()
+            .join('\n')
+        );
       })
       .catch(error => msg.send(error.message));
   });
