885b41a1ae88c4346b60fa2976b6c077bbf24828
kotlarz <marius.kotlarz@gmail.com>
2019-08-23 13:13:17 +0200

    Add rave command (to control the lasers)

---
+++ scripts/rave.js
@@ -0,0 +1,45 @@
+// Description:
+//   Control the lasers at our office (rave!)
+// Configuration:
+//   OPENFAAS_URL,
+// Commands
+//   hubot rave on - Turn on the lasers
+//   hubot rave off - Turn off the lasers
+//   hubot rave lock - Lock the power state of the lasers so it cannot be changed by other applications
+//   hubot rave unlock - Unlock the power state of the lasers so it can be changed by other applications
+
+const _ = require('lodash');
+const fetch = require('node-fetch');
+const openFaas = require('../lib/openfaas');
+
+function sendCommand(command) {
+  payload = {
+    command
+  };
+
+  return openFaas('office-lasers-api', {
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }).then(response => {
+    return;
+  });
+}
+
+module.exports = robot => {
+  robot.respond(/rave off/i, msg => {
+    const send = msg.send.bind(msg);
+    sendCommand('force_power_off').catch(error => send(error.message));
+  });
+  robot.respond(/rave on/i, msg => {
+    const send = msg.send.bind(msg);
+    sendCommand('force_power_on').catch(error => send(error.message));
+  });
+  robot.respond(/rave lock/i, msg => {
+    const send = msg.send.bind(msg);
+    sendCommand('power_lock').catch(error => send(error.message));
+  });
+  robot.respond(/rave unlock/i, msg => {
+    const send = msg.send.bind(msg);
+    sendCommand('power_unlock').catch(error => send(error.message));
+  });
+};
