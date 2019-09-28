93a19baf434837f9385a764b5ab94d34116a7391
Peder Smith <smith.peder@gmail.com>
2019-09-28 17:33:21 +0200

    Revert "Test quick logging"
    This reverts commit 48a26d9fad314e595ffdc89d41d2ec08232448d6.

--- scripts/tv.js
+++ scripts/tv.js
@@ -15,7 +15,6 @@
 const _ = require('lodash');
 const fetch = require('node-fetch');
 const openFaas = require('../lib/openfaas');
-const log = require('./log');
 
 // Django regex for URLs
 const regexUrlCommand = /tv (.*(?:http|ftp)s?:\/\/(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+).*)/i;
@@ -41,7 +40,6 @@ module.exports = robot => {
     });
   });
   robot.respond(/tv play/i, msg => {
-    log(msg);
     const send = msg.send.bind(msg);
     sendCommand('play').catch(error => send(error.message));
     robot.adapter.client.web.reactions.add('arrow_forward', {
