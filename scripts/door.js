6a129adabf071cb3882527de6b1fbc253ee363b3
Peder Smith <smith.peder@gmail.com>
2019-10-13 14:00:10 +0200

    Try Sentry

--- scripts/door.js
+++ scripts/door.js
@@ -8,9 +8,21 @@ const _ = require('lodash');
 const officeDoor = require('../lib/office_door');
 const logger = require('../lib/log');
 
+const { SENTRY_DSN } = process.env;
+const Sentry = require('@sentry/node');
+
 module.exports = robot => {
   robot.respond(/door/i, msg => {
     logger.log(msg);
+
+    // TEST SENTRY ERRORS
+    Sentry.init({ dsn: SENTRY_DSN });
+    console.log('DSN: ', SENTRY_DSN);
+    Sentry.captureMessage('Do you even work?');
+    console.log('The lastEventId is:', Sentry.lastEventId());
+    undefinedFunction();
+    // TEST SENTRY ERRORS
+
     // Reply with the status of the door
     officeDoor()
       .then(door => {
